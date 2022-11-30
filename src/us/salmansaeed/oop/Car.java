c9fddab8934a794d56080f397163eae2a8de4821
algosal <pfc.salman@gmail.com>
2022-11-30 08:03:33 -0500

    Object Orientation Programming OOP Started

---
+++ src/us/salmansaeed/oop/Car.java
@@ -0,0 +1,35 @@
+package us.salmansaeed.oop;
+
+public class Car {
+	private String door;
+	private String engine;
+	private String driver;
+	private int speed;
+	
+	public String getDoor() {
+		return door;
+	}
+	public void setDoor(String door) {
+		this.door = door;
+	}
+	public String getEngine() {
+		return engine;
+	}
+	public void setEngine(String engine) {
+		this.engine = engine;
+	}
+	public String getDriver() {
+		return driver;
+	}
+	public void setDriver(String driver) {
+		this.driver = driver;
+	}
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	
+	
+}
---
+++ src/us/salmansaeed/oop/CarMain.java
@@ -0,0 +1,12 @@
+package us.salmansaeed.oop;
+
+public class CarMain {
+
+	public static void main(String[] args) {
+		Car car = new Car();
+		car.setSpeed(10);
+		System.out.println(car.getSpeed());
+		
+	}
+
+}
