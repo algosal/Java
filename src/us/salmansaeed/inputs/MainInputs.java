f05cd7ee3e8b69f18c23a11d73d58d859fd43287
algosal <pfc.salman@gmail.com>
2022-12-05 08:15:39 -0500

    Java Inputs

---
+++ src/us/salmansaeed/inputs/MainInputs.java
@@ -0,0 +1,19 @@
+package us.salmansaeed.inputs;
+
+import java.util.Scanner;
+
+public class MainInputs {
+
+	public static void main(String[] args) {
+		System.out.println("Please Enter your Name? ");
+		Scanner sc = new Scanner(System.in);
+		String name = sc.nextLine();
+		while(!name.matches("[a-zA-Z]+")){
+		    System.out.println("Please enter a valid name!");
+		    name = sc.nextLine();
+		}
+		System.out.println("Your Name is " + name);
+		sc.close();
+	}
+
+}
