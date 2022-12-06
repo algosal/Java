ea4455a32c071d357bc824142205d8f22c3675c5
algosal <pfc.salman@gmail.com>
2022-12-06 17:20:07 -0500

    Packages and Fully Qualified Classes with Same Name of Class

---
+++ src/us/salmansaeed/packages/PakacgesSample.java
@@ -0,0 +1,17 @@
+package us.salmansaeed.packages;
+import javax.swing.plaf.basic.BasicComboBoxUI.ListDataHandler;
+
+public class PakacgesSample {
+
+	ListDataHandler list;
+	
+	/**
+	 * Fully Qualified Class Names for Distinguishability
+	 * We do not need the import statement with 
+	 * Fully Qualified name of the class
+	 */
+	javax.swing.plaf.basic.BasicComboPopup.ListDataHandler listP;
+	javax.swing.plaf.basic.BasicListUI.ListDataHandler listUI; 
+	
+	
+}
