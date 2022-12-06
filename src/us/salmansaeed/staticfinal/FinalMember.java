b59cf4a752d02fd5184a5631bed4b1fb7e2236dd
algosal <pfc.salman@gmail.com>
2022-12-06 09:32:30 -0500

    Final Member class and the comments to understand the final keyword

---
+++ src/us/salmansaeed/staticfinal/FinalMember.java
@@ -0,0 +1,12 @@
+package us.salmansaeed.staticfinal;
+
+public class FinalMember {
+
+	public final int x;
+	public FinalMember(int x) {
+		this.x = x;
+	}
+	public int getX() {
+		return x;
+	}
+}
--- src/us/salmansaeed/staticfinal/SaticFinalMain.java
+++ src/us/salmansaeed/staticfinal/SaticFinalMain.java
@@ -3,8 +3,24 @@ package us.salmansaeed.staticfinal;
 public class SaticFinalMain {
 
 	public static void main(String[] args) {
+		/**
+		 * final class cannot be extended
+		 */
+		
 		FinalClass fc = new FinalClass();
 		fc.hello();
+		
+		/**
+		 * final members can be initialized by a constructor 
+		 * and that is why they are not the constants
+		 */
+		
+		FinalMember fm = new FinalMember(100);
+		System.out.println(fm.getX());
+		
+		/**
+		 * final methods cannot be overriden 
+		 */
 
 	}
 
